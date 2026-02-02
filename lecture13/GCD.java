d5332d79fd15409d149933191c400a1f27b42158
Devendan <solankidevendan@gmail.com>
2026-02-02 20:14:41 +0530

    gcd

---
+++ lecture13/GCD.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class GCD{
+    public static void main (String[] args ){
+        Scanner sc = new  Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        while(b != 0 ){
+            int t = (a%b);
+            a = b ;
+            b = t ;
+        }
+
+        System.out.println(a);
+
+    }
+}
