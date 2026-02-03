c6743c512d77f95175ba47cd49797190c1ac70d9
Devendan <solankidevendan@gmail.com>
2026-02-03 13:48:02 +0530

    prime

---
+++ lecture 14/prime.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class prime {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        boolean flag = false ;
+        for( int i= 2 ; i< n/2 ; i++){
+            if(n % i == 0){
+                 flag = false ;
+                 break ;
+            }
+            else{
+                flag = true ;
+            }
+        }
+
+        if(flag){ 
+            System.out.println("Prime ");
+        }else{
+            System.out.println("not Prime ");
+        }
+    }
+    
+}
